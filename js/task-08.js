45fa6bf57b2d67ede993357763a2bddd224c0a66
Daniel Santa Mateus <dsm50561@hotmail.com>
2023-05-13 20:03:44 -0500

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const {
+        email: { value: mailUser },
+        password: {value: passwordUser},
+    } = loginForm.elements;
+
+    const infoUser = { email: mailUser, password: passwordUser };
+
+    if (mailUser === "" || passwordUser === "") {
+        alert("Todos los espacios deben ser llenados")
+        return;
+    }
+    console.log(infoUser);
+    e.currentTarget.reset();
+});
