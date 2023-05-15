339fe75118c6bb4dcba77664a69400899c1d973d
Daniel Santa Mateus <dsm50561@hotmail.com>
2023-05-15 15:36:03 -0500

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const controlsInput = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  let width = 30;
+  let height = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const newBox = document.createElement("div");
+    newBox.classList.add("box");
+    boxes.append(newBox);
+    newBox.style.width = `${width}px`;
+    newBox.style.height = `${height}px`;
+    newBox.style.backgroundColor = getRandomHexColor();
+
+    width += 10;
+    height += 10;
+  }
+}
+
+function destroyBoxes() {
+  const boxElement = document.querySelectorAll(".box");
+  boxElement.forEach(box => box.remove());
+};
+
+createBtn.addEventListener("click", () => {
+  const newElements = controlsInput.valueAsNumber;
+  if (newElements) {
+    destroyBoxes();
+  }
+  createBoxes(newElements);
+});
+
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+});
+
